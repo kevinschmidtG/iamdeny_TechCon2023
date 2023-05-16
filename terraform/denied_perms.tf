4fb3f1304c979d421d1f5cf8a6061b0a8c72add3
kevinschmidtG <kschmidt@apple.com>
2023-05-15 23:45:28 -0500

    adding in license everywhere

--- terraform/denied_perms.tf
+++ terraform/denied_perms.tf
@@ -1,5 +1,18 @@
+    # Copyright 2023 Google LLC
+
+    # Licensed under the Apache License, Version 2.0 (the "License");
+    # you may not use this file except in compliance with the License.
+    # You may obtain a copy of the License at
+
+    #     https://www.apache.org/licenses/LICENSE-2.0
+
+    # Unless required by applicable law or agreed to in writing, software
+    # distributed under the License is distributed on an "AS IS" BASIS,
+    # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    # See the License for the specific language governing permissions and
+    # limitations under the License.
+
 #Broad list of permissions to deny 
-#Please check 
 locals {
   project_admin_perms_deny = [
     # client OAuth2 clients
@@ -92,7 +105,6 @@ locals {
     "iam.googleapis.com/workloadIdentityPools.delete",
     "iam.googleapis.com/workloadIdentityPools.undelete",
     "iam.googleapis.com/workloadIdentityPools.update",
-    # project mutable
     "cloudresourcemanager.googleapis.com/projects.createBillingAssignment", #v2, in v1 this was just resourcemanager
     "cloudresourcemanager.googleapis.com/projects.delete",
     "cloudresourcemanager.googleapis.com/projects.deleteBillingAssignment",
